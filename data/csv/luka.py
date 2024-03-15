fb401851e3c85689b2e41022da99777b0180f41c
jayhemrajani <143853771+jayhemrajani@users.noreply.github.com>
2024-03-15 02:52:57 -0400

    Update luka.py

--- data/csv/luka.py
+++ data/csv/luka.py
@@ -1,1 +1,4 @@
-print("theres gotta be a better way to do this")
+#open SPECIFIC file (for test purposes, of course) and print out contents of file
+with open('2022-2023.csv', 'r') as file:
+    contents = file.read()
+    print(contents)
